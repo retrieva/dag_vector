452bda727f27d8a170a132073db5c8b54711eab0
Daisuke OKanohara <daisuke.okanohara@gmail.com>
2011-06-20 01:24:47 +0900

    add bv performance test

--- test/all_select_performance_test.cpp
+++ test/all_select_performance_test.cpp
@@ -18,6 +18,7 @@ int main(int argc, char* argv[]){
 
   vector<uint64_t> vals;
   dag::dag_vector dagv;
+  dag::bit_vector bv;
   dag::sparse_vector ds(size, maxval / 2 * size);
   uint64_t sum = 0;
   for (int i = 0; i < size; ++i){
@@ -25,16 +26,19 @@ int main(int argc, char* argv[]){
     sum += v;
     vals.push_back(sum);
     dagv.push_back(v);
+    for (uint64_t j = 0; j < v; ++j){
+      bv.push_back(0, 1);
+    }
+    bv.push_back(1, 1);
     ds.set_next_bit(sum);
   }
 
-  cout << "size" << endl;
   cout << "vals:" << vals.size() * sizeof(uint64_t) << endl;
   cout << "dagv:" << dagv.get_alloc_byte_num() << endl;
+  cout << "  bv:" << bv.get_alloc_byte_num() << endl;
   cout << "  ds:" << ds.get_alloc_byte_num() << endl;
 
   bgroup("sequeantial") {
-    /*
     bench("vector"){
       for (size_t i = 0; i < vals.size(); ++i){
         sum += vals[i];
@@ -54,13 +58,20 @@ int main(int argc, char* argv[]){
       }
     }
 
+    bench("bit vector getbit"){
+      for (size_t i = 0; i < bv.size(); ++i){
+        if (bv.get_bit(i)){
+          sum += i;
+        }
+      }
+    } 
+
     bench("sparse_vector select"){
       for (size_t i = 0; i < ds.one_num(); ++i){
         sum += ds.select(i);
       }
     }
 
-    */
     bench("sparse_vector iterator"){
       dag::sparse_vector::const_iterator end = ds.end();
       dag::sparse_vector::const_iterator it = ds.begin();
